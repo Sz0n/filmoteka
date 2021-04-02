1737763878b47e2840e85af196719fa7aee200aa
sz0n <kamilrsw@gmail.com>
2021-04-02 12:42:42 +0200

    add role restriction for fill_db view

--- filmoteka/website/urls.py
+++ filmoteka/website/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.contrib.admin.views.decorators import staff_member_required
 from .views import FillDB, main, show_movie, movies_list, favourites
 
 urlpatterns = [
     path('', main, name='home'),
-    path('fill_db/', FillDB.as_view(), name='fill_db'),
+    path('fill_db/', staff_member_required(FillDB.as_view()), name='fill_db'),
     path('movies', movies_list, name='movies_list'),
     path('movies/<str:title>', show_movie, name='show_movie'),
     path('favourites', favourites, name='favourites')
